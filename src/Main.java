afe8ddd883a798b9d4d866b9664ea32b1b1c1ad9
Peter <spagootergoo@gmail.com>
2022-09-26 20:18:37 -0700

    i am so epic, Right?

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Monkey");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH );
+        System.out.println("I am A dingus");
+    }
+}
